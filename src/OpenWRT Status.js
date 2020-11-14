dcf9f2e47795bd8321acf1008213563b5496f727
Chris Mare <chris@mare.org.uk>
2020-11-14 15:11:33 +0000

    Added stacks to openwrt status

--- src/OpenWRT Status.js
+++ src/OpenWRT Status.js
@@ -8,15 +8,21 @@
 const baseurl = Keychain.get('openwrt.url');
 const passwd = Keychain.get('openwrt.pass');
 
-// Customise your interface name 
+// Customise settings
 const iface = 'eth0.2';
+const loadHigh = 2; // Load will depend on your CPU cores
+const flashHigh = 75; // percent
+const memHigh = 75; // percent
+
+// Don't change anything below
+// ===========================
 
 let data = {};
 let token = null;
 try {
   token = await rpcCall('auth', 'login', ['root', passwd]);
   data = await getData();
-} catch(e) {
+} catch (e) {
   console.error(e);
   console.log(baseurl);
 }
@@ -33,7 +39,7 @@ Script.complete();
 
 function createWidget(data) {
   const widget = new ListWidget();
-  widget.spacing = 2;
+  widget.spacing = 1;
   const now = new Date().getTime();
   widget.refreshAfterDate = new Date(now + 60000);
   const gradient = new LinearGradient();
@@ -44,53 +50,108 @@ function createWidget(data) {
   wdate.textColor = Color.gray();
   wdate.rightAlignText();
   wdate.applyOffsetStyle();
-  wdate.font = Font.systemFont(12);
-  const header = addText(widget, 'OpenWRT');
-  header.font = Font.boldSystemFont(22);
+  wdate.font = Font.systemFont(8);
+  const header = addText(widget, '⚡️OpenWRT');
+  header.font = Font.boldSystemFont(14);
   if (token === null) {
     const nodata = addText(widget, 'NO DATA');
     nodata.font = Font.boldSystemFont(16);
     nodata.textColor = Color.red();
     return widget;
   }
-  addText(widget, `LOAD: ${data.load}`, Color.orange());
-  addText(
-    widget,
-    `NET: ↑${data.transmitted} GB ↓${data.received} GB`,
+
+  widget.addSpacer(3);
+  const stack1 = widget.addStack();
+  stack1.layoutHorizontally();
+  stack1.spacing = 4;
+  addStat(
+    stack1,
+    'LOAD',
+    data.load.join('/'),
+    data.load[2],
+    Color.orange(),
+    parseFloat(data.load[2]) >= loadHigh
+  );
+  stack1.addSpacer(2);
+  addStat(
+    stack1,
+    'NET',
+    `↑${data.transmitted} GB`,
+    `↓${data.received} GB`,
     Color.cyan()
   );
-  addText(
-    widget,
-    `MEM: ${data.used}/${data.total} MB, ${data.percent}%`,
-    Color.magenta()
+  widget.addSpacer(4);
+  const stack2 = widget.addStack();
+  stack2.spacing = 4;
+  stack2.layoutHorizontally();
+  addStat(
+    stack2,
+    'MEM',
+    `${data.used}/${data.total} MB`,
+    `${data.percent}%`,
+    Color.magenta(),
+    data.percent >= memHigh
   );
-  addText(
-    widget,
-    `FLASH: ${data.flashUsed}/${data.flashTotal} MB, ${data.flashPercent}%`,
-    Color.blue()
+  stack2.addSpacer(2);
+  addStat(
+    stack2,
+    'FLASH',
+    `${data.flashUsed}/${data.flashTotal} MB`,
+    `${data.flashPercent}%`,
+    Color.blue(),
+    data.flashPercent >= flashHigh
   );
+  widget.addSpacer(3);
   addUptime(widget, data.uptime);
-  addText(widget, `IP: ${data.ip}`, Color.green());
+  addText(widget, ` IP: ${data.ip}`, Color.yellow());
   return widget;
 }
 
 function addText(container, text, color = Color.white()) {
   const wtext = container.addText(text);
-  wtext.font = Font.regularSystemFont(11);
+  wtext.font = Font.regularSystemFont(8);
   wtext.textColor = color;
   return wtext;
 }
 
 function addUptime(widget, uptime) {
   const stack = widget.addStack();
-  addText(stack, 'UP: ', Color.yellow());
+  addText(stack, ' UP: ', Color.yellow());
   const currentMs = new Date().getTime();
   const startMs = currentMs - uptime * 1000;
   const started = new Date(startMs);
   const wdate = stack.addDate(started);
   wdate.applyRelativeStyle();
-  wdate.font = Font.regularSystemFont(11);
+  wdate.font = Font.regularSystemFont(8);
   wdate.textColor = Color.yellow();
+}
+
+function addStat(container, label, footnote, value, color, isHigh) {
+  const stack = container.addStack();
+  stack.size = new Size(70, 45);
+  stack.borderColor = color;
+  stack.borderWidth = 4;
+  stack.cornerRadius = 8;
+  stack.layoutVertically();
+  stack.setPadding(4, 4, 4, 4);
+
+  const wlabel = stack.addText(label);
+  wlabel.font = Font.mediumSystemFont(12);
+  wlabel.textColor = color;
+  wlabel.centerAlignText();
+
+  if (footnote) {
+    const wfootnote = stack.addText(footnote);
+    wfootnote.font = Font.mediumSystemFont(8);
+    wfootnote.textColor = Color.white();
+    wfootnote.centerAlignText();
+  }
+  if (value) {
+    const wvalue = stack.addText(value);
+    wvalue.font = Font.mediumSystemFont(12);
+    wvalue.textColor = isHigh ? Color.red() : Color.green();
+    wvalue.centerAlignText();
+  }
 }
 
 async function getData() {
@@ -114,7 +175,7 @@ async function getData() {
   const trafficRegex = /received +(\d+).*transmitted +(\d+)/s;
   const [, receivedStr, transmittedStr] = trafficRegex.exec(trafficRaw) || [];
   console.log(receivedStr);
-  const load = loadRaw.split(' ', 3).join('/');
+  const load = loadRaw.split(' ', 3);
   const meminfo = parseMeminfo(meminfoRaw);
   const flashTotal = parseInt(flashTotalStr);
   const flashUsed = parseInt(flashUsedStr);
